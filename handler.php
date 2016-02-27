bf6029dd1fa6075415d12b1df81b2ffa97bd9cdd
botsko <viktorgomonec@i.ua>
2016-02-27 16:30:04 +0200

    калькулятор

---
+++ handler.php
@@ -0,0 +1,21 @@
+<?php
+$result = '-';
+if (isset($_REQUEST["calc"])) {
+    $a = $_REQUEST ['a'];
+    $b = $_REQUEST ['b'];
+    $operation = $_REQUEST ['operation'];
+    switch ($operation) {
+        case '+':
+            $result = $a + $b;
+            break;
+        case '-':
+            $result = $a - $b;
+            break;
+        case '*':
+            $result = $a * $b;
+            break;
+        case '/':
+            $result = $a / $b;
+            break;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo "прив"
+include 'handler.php'
 ?>
+<html>
+<head>
+</head>
+<body>
+<form method="post">
+    <label>
+        <input type="text" value="<?= $_REQUEST ['a'] ?>" name="a">
+    </label>
+    <label>
+        <select name="operation">
+            <option <?=$_REQUEST['operation']=='+'?'selected="selected"': ''?> value="+">+</option>
+            <option <?=$_REQUEST['operation']=='-'?'selected="selected"': ''?>value="-">-</option>
+            <option <?=$_REQUEST['operation']=='*'?'selected="selected"': ''?>value="*">*</option>
+            <option <?=$_REQUEST['operation']=='/'?'selected="selected"': ''?> value="/">/</option>
+        </select>
+    </label>
+    <label>
+        <input type="text" value="<?= $_REQUEST ['b'] ?>" name="b">
+    </label>
+    <span>=</span>
+    <span><?= $result ?></span>
+    <button type="submit" value="1" name="calc">OK</button>
+</form>
+</body>
+</html>
